a882b241289c3ea5e6427acccbbe9b9c7f8f4eb4
neelkirit <neelkirit2@gmail.com>
2019-05-15 18:14:20 -0400

    Production Build

--- src/config/key.js
+++ src/config/key.js
@@ -5,10 +5,3 @@ export const MeetupAPI = {
 
 
 
-// PASTE_YOUR_MEETUP_API_KEY_HERE
-
-
-// MEETUP_API_KEY
-// For apiKey: https://secure.meetup.com/meetup_api/key/
-// For urlname: extract the urlname from meetup chapter link-> https://meetup.com/GDG-Jalandhar
-// Only Organizer / Co-Organizer can get the Meetup API Key
